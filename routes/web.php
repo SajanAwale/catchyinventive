54efc03b13d8f290f746b81f12760197b53a4c7a
bidolive <bidya.singh@olivegroup.io>
2024-12-23 21:02:12 +0545

    CIN-28 admin panel setup implemented

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\admin\AdminLoginController;
+use App\Http\Controllers\admin\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin/login',[AdminLoginController::class,'index'])->name('admin.login');
+Route::post('/admin/authenticate',[AdminLoginController::class,'authenticate'])->name('admin.authenticate');
+
+Route::get('/admin/dashboard',[DashboardController::class,'index'])->name('admin.dashboard');
