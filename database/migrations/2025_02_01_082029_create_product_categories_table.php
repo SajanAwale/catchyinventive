a72382b8d6af99d9fc9e5a9344b65a69ba3216bb
SajanAwale <sajanawale@gmail.com>
2025-03-16 21:06:31 +0545

    Improvement: Create a seeder for adding role to ensure no duplicate role is added

--- database/migrations/2025_02_01_082029_create_product_categories_table.php
+++ database/migrations/2025_02_01_082029_create_product_categories_table.php
@@ -11,8 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('product_categories', function (Blueprint $table) {
+        Schema::create('product_category', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('parent_category_id');
             $table->timestamps();
         });
     }
@@ -22,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('product_categories');
+        Schema::dropIfExists('product_category');
     }
 };
--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RoleSeeder extends Seeder
 {
@@ -12,6 +13,20 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Ensure no duplicate roles
+        DB::table('roles')->insertOrIgnore([
+            [
+                'id' => 1,
+                'name' => 'Admin',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'id' => 2,
+                'name' => 'Customer',
+                'created_at' => now(),
+                'updated_at' => now()
+            ]
+        ]);
     }
 }
