f5105b218ae568d01797bdf7d5cce2fa1b9b1b04
SajanAwale <sajanawale@gmail.com>
2025-05-27 23:16:01 +0545

    Improvement: update brand name spelling

--- app/Models/brand.php
+++ app/Models/brand.php
@@ -6,11 +6,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+
 class Brand extends Model
 {
     /** @use HasFactory<\Database\Factories\BrandFactory> */
     use HasFactory;
     use SoftDeletes;
+
+    protected $table = 'brands';
 
     protected $fillable = [
         'name',
@@ -28,7 +31,7 @@ class Brand extends Model
 
     protected $casts = [
         'name' => 'string',
-        'slut' => 'string',
+        'slug' => 'string',
         'image' => 'string',
         'permalink' => 'string',
         'description' => 'string',
