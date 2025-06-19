2ebb8c55b1e1190ef29eab6b2d7796e2edfb1c51
SajanAwale <sajanawale@gmail.com>
2025-06-19 21:04:45 +0545

    Improvement: Cleaning unnecessary codes

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/admin/login',[AdminLoginController::class,'index'])->name('admin.login');
-Route::post('/admin/authenticate',[AdminLoginController::class,'authenticate'])->name('admin.authenticate');
+// Route::get('/admin/login',[AdminLoginController::class,'index'])->name('admin.login');
+// Route::post('/admin/authenticate',[AdminLoginController::class,'authenticate'])->name('admin.authenticate');
 
-Route::get('/admin/dashboard',[DashboardController::class,'index'])->name('admin.dashboard');
+// Route::get('/admin/dashboard',[DashboardController::class,'index'])->name('admin.dashboard');
 // Route::get('/', function () {
 //     return view('welcome');
 // });
